9f84eb4d19d4a726403b8ca632254cce891a47cc
Arthur <arthurcaco@gmail.com>
2023-05-18 21:54:38 -0300

    Inital commit

---
+++ src/principal/Program.java
@@ -0,0 +1,21 @@
+package principal;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//mais uma utilizada quando ja sabemos
+		//a quantidade de vezes que precisamos
+		//repetir comandos
+		
+		/*SINTAXE DO FOR 
+		*1° variável que inicia o contador;
+		*2° condição que deve ser aceita;
+		*3° incremento
+		*/
+	 for(int contador = 0; contador < 1000000; contador++) {
+		 System.out.println("valor de contador: "+contador);
+	 }
+	
+	}	
+
+}
