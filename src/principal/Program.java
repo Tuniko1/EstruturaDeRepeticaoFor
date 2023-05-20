83b88e8832209f729c6195477af8e5de95e8f42a
Arthur <arthurcaco@gmail.com>
2023-05-19 21:02:05 -0300

    mostra os números ímpares até 25

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -12,10 +12,21 @@ public class Program {
 		*2° condição que deve ser aceita;
 		*3° incremento
 		*/
-	 for(int contador = 0; contador < 1000000; contador++) {
-		 System.out.println("valor de contador: "+contador);
-	 }
+		
+		/*1 Programa em Java que mostra os números ímpares ate 25
+		* 2 Altere o primeiro programa para mostrar a soma dos números impares
+		* 3 altere novamente o programa para mostrar os números impares um abaixo do outro e depois um ao lado do outro
+		*/
+		 
+	 for (int contador = 0; contador <= 25; contador++){
+		 if (contador % 2 != 0 ) {
+	     
+		 System.out.print(contador+" ");
+		 }
+		}
+	 
+	}
 	
-	}	
+	
 
 }
