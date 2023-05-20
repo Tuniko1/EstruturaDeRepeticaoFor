892440696f78b614a2daaf201b4b17173fc8893b
Arthur <arthurcaco@gmail.com>
2023-05-19 21:04:26 -0300

    Soma dos números ímpares

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -18,13 +18,17 @@ public class Program {
 		* 3 altere novamente o programa para mostrar os números impares um abaixo do outro e depois um ao lado do outro
 		*/
 		 
-	 for (int contador = 0; contador <= 25; contador++){
-		 if (contador % 2 != 0 ) {
-	     
-		 System.out.print(contador+" ");
-		 }
-		}
-	 
+		 int soma = 0;
+		 for (int contador = 0; contador <= 25; contador++){
+			 if (contador % 2 != 0 ) {
+		     
+			 System.out.print(contador+" + ");
+			 }
+			 
+		 if (contador % 2 != 0 && contador == 25) {
+			 soma += contador ;
+			 System.out.println("= "+soma);
+		 }}
 	}
 	
 	
