c5ecb5637b384a079d989d17baceb07dab99e175
Arthur <arthurcaco@gmail.com>
2023-05-19 21:36:43 -0300

    mostrar números impares um abaixo do outro depois um ao lado do outro

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -17,18 +17,26 @@ public class Program {
 		* 2 Altere o primeiro programa para mostrar a soma dos números impares
 		* 3 altere novamente o programa para mostrar os números impares um abaixo do outro e depois um ao lado do outro
 		*/
-		 
 		 int soma = 0;
-		 for (int contador = 0; contador <= 25; contador++){
+		 
+		for (int contador = 0; contador <= 25; contador++){
 			 if (contador % 2 != 0 ) {
-		     
-			 System.out.print(contador+" + ");
-			 }
+			 		 
+			 System.out.println(contador);
+			 		 
+			 }	 
+		 }
+		for (int contador = 0; contador <= 25; contador++){
+			 if (contador % 2 != 0 ) {
+				 
+				 soma += contador;
+				 System.out.print(contador+" + ");
+			 }if(contador == 25) {
+				 
+				 System.out.println(" = "+soma);
 			 
-		 if (contador % 2 != 0 && contador == 25) {
-			 soma += contador ;
-			 System.out.println("= "+soma);
-		 }}
+			 }}
+		
 	}
 	
 	
